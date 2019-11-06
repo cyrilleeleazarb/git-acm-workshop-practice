4480fc34046fc30ac943628d3bc6f3a582f3cdd7
Cyrille Bernabe <cyrillebernabe@Cyrilles-MacBook-Pro.local>
2019-11-05 19:17:17 -0800

    Testing commit message

---
+++ main.c
@@ -0,0 +1,18 @@
+//Main.c for git
+
+#include <stdio.h>
+
+float math(float x)
+{
+    return 2*x;
+}
+
+int main()
+{
+    int a, b;
+    float x;
+
+    printf("Enter your bounds: ");
+    
+    return 0;
+}
